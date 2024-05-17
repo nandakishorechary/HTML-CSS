c7c0858bc835ebcf835ca28b8619db0913d6c3dc
nandakishorechary <nandakishorechary2483@gmail.com>
2024-05-17 18:40:50 +0530

    update folder

--- myscript.js
+++ myscript.js
@@ -7,19 +7,10 @@ function login(){
         document.write("acces denied");
     }
 }
-function page1(){
-    const page1=document.getElementById("p1");
-    const page2=document.getElementById("p2").classList(
-        "active"
-    );
-    
-    page2.classList.add('show');
-   
-}
-function page2(){
-    const page1=document.getElementById("p1");
-    const page2=document.getElementById("p2");
-    page1.classList.remove('hide');
-    page2.classList.remove('show');
 
-}
+// function f2(){
+//     const page1=document.getElementById("p1");
+//     const page2=document.getElementById("p2");
+//     page1.classList.remove('hide');
+//     page2.classList.remove('show');
+// }
