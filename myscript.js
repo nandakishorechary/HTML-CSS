c748d5c0160a97b972afe5206f8ff214b3422d55
nandakishorechary <nandakishorechary2483@gmail.com>
2024-05-22 18:38:23 +0530

    added files

--- myscript.js
+++ myscript.js
@@ -7,7 +7,18 @@ function login(){
         document.write("acces denied");
     }
 }
+const show = () => {
+    const form = document.querySelector("p2")
+    form.classList.toggle("show")
+    document.querySelector(".layer").style.display="block"
+   
+}
 
+const hide = () => {
+    const hide = document.querySelector(".p2")
+    hide.classList.remove("show")
+    document.querySelector(".layer").style.display="none"
+}
 // function f2(){
 //     const page1=document.getElementById("p1");
 //     const page2=document.getElementById("p2");
